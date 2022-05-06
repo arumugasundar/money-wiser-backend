38c45cb0d5f7541447da558427ff9ab566c2eb9c
arumugasundar <arumugasundar29@gmail.com>
2022-05-06 10:16:00 +0530

    App Setup and updates to deploy in heroku

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+const app = express();
+// const MERGE_PDFS_API = require('./routes/merge-pdfs-api');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use((req,res,next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers","*");
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, PUT, DELETE, OPTIONS")
+    next();
+});
+
+app.get('/', (req,res,next) => {
+    res.send('money-wiser-backend app is running!');
+});
+
+// app.use('/api/merge-pdfs',MERGE_PDFS_API);
+
+module.exports = app;
